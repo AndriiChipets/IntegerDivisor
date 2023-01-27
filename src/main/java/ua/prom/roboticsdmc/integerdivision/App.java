5607e7408ebe731258306024527d9e2b0dc4d819
Andrii Chipets <anchrobotics@gmail.com>
2023-01-27 15:31:36 +0200

    Initial commit

---
+++ src/main/java/ua/prom/roboticsdmc/integerdivision/App.java
@@ -0,0 +1,11 @@
+package ua.prom.roboticsdmc.integerdivision;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
